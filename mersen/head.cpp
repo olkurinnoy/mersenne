3dc7f4be3741d77540ca4b4dda75032188bff9d7
Oleg <ol.kurinnoy@gmail.com>
2018-03-27 21:46:04 +0300

    basic implementation and base of the future project

---
+++ mersen/head.cpp
@@ -0,0 +1,6 @@
+#include "head.h"
+
+int add(int x, int y)
+{
+	return x*2+y*2;
+}
---
+++ mersen/main.cpp
@@ -0,0 +1,13 @@
+#include "gmp.h"
+#include <iostream>
+#include <openssl/sha.h>
+#include "head.h"
+using namespace std;
+
+int main()
+{
+	mpz_t a;
+	
+	cin.get();
+	return 0;
+}
